10a8e72088b6ef68885594d5a3e5e863e9d3d7d8
younthing <fanxingfu3344@gmail.com>
2024-09-04 17:01:19 +0800

    feat: init default config.yaml

---
+++ biorange/core/__init__.py
@@ -0,0 +1,5 @@
+"""# 当pwd没有config.yaml的时候，复制一份"""
+
+from biorange.core.utils.package_fileload import copy_config_if_not_exists
+
+copy_config_if_not_exists()
--- biorange/core/utils/package_fileload.py
+++ biorange/core/utils/package_fileload.py
@@ -1,5 +1,9 @@
 from importlib import resources
 
+from biorange.core.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 def get_data_file_path(filename):
     """
@@ -16,3 +20,23 @@ def get_data_file_path(filename):
     """
     with resources.path("biorange.data", filename) as path:
         return path
+
+
+import os
+from shutil import copyfile
+
+
+def copy_config_if_not_exists(target_dir=".", filename="config.yaml"):
+    """
+    如果目标目录不存在指定文件，则从包内复制该文件。
+
+    Args:
+        target_dir (str): 目标目录路径，默认为当前目录。
+        filename (str): 要复制的文件名。
+    """
+    target_file = os.path.join(target_dir, filename)
+    if not os.path.exists(target_file):
+        logger.warning(f"未找到 {filename}，新建默认配置config.yaml...")
+        with resources.path("biorange.data", filename) as path:
+            copyfile(path, target_file)
+            logger.info(f"{filename} 配置成功")
